2a93c581b9c23255c176e6057392bf9a335b68f0
Afonso Remédios <afonso.remedios@isel.pt>
2023-11-08 22:58:35 +0000

    Aula 14 (2023/11/08)

--- Galo/src/main/kotlin/Main.kt
+++ Galo/src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
 import isel.tds.galo.model.*
+import isel.tds.galo.mongo.MongoDriver
 import isel.tds.galo.storage.BoardSerializer
 import isel.tds.galo.storage.GameSerializer
+import isel.tds.galo.storage.MongoStorage
 import isel.tds.galo.storage.TextFileStorage
 import isel.tds.galo.view.getCommands
 import isel.tds.galo.view.readCommandLine
@@ -9,26 +11,28 @@ import java.lang.IllegalArgumentException
 import java.lang.IllegalStateException
 
 fun main() {
+    MongoDriver("galo").use { driver ->
+        var game: Game = Game()
+//    val storage = TextFileStorage<String, Game>("saves", GameSerializer)
+        val storage = MongoStorage<String, Game>("saves", driver, GameSerializer)
+        val commands = getCommands(storage)
 
-    var game: Game = Game()
-    val storage = TextFileStorage<String, Game>("saves", GameSerializer)
-    val commands = getCommands(storage)
-    while(true){
-        val (name,args)  = readCommandLine()
-        val cmd = commands[name]
-        if(cmd==null)println("Invalid Command $name")
-        else
-            try {
-                if( cmd.isToFinish) break
-                game = cmd.execute(args, game)
-            }catch (e: IllegalStateException){
-                println(e.message)
-            }
-            catch (e: IllegalArgumentException){
-                println(e.message)
-            }
-        game.show()
+        while (true) {
+            val (name, args) = readCommandLine()
+            val cmd = commands[name]
+            if (cmd == null) println("Invalid Command $name")
+            else
+                try {
+                    if (cmd.isToFinish) break
+                    game = cmd.execute(args, game)
+                } catch (e: IllegalStateException) {
+                    println(e.message)
+                } catch (e: IllegalArgumentException) {
+                    println(e.message)
+                }
+            game.show()
 
+        }
     }
 }
 
--- Galo/src/main/kotlin/isel/tds/galo/view/Command.kt
+++ Galo/src/main/kotlin/isel/tds/galo/view/Command.kt
@@ -1,6 +1,7 @@
 package isel.tds.galo.view
 
 import isel.tds.galo.model.*
+import isel.tds.galo.storage.Storage
 import isel.tds.galo.storage.TextFileStorage
 
 abstract class Command( val argSyntax: String = "") {
@@ -21,7 +22,7 @@ object Play : Command("idx" ){
 
 }
 
-fun getCommands(storage: TextFileStorage<String, Game>): Map<String, Command> {
+fun getCommands(storage: Storage<String, Game>): Map<String, Command> {
     return mapOf<String, Command>(
         "PLAY" to Play,
         "NEW" to object : Command(){
